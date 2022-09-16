34d3af98a4237ab8646d2c897099c7b369c9a106
NotLewUwU <abderrahmanlabiod@iessantiagohernadez.com>
2022-09-16 15:39:39 +0200

    Initial commit

---
+++ src/main/java/me/notlewx/bossbar/Bossbar.java
@@ -0,0 +1,30 @@
+package me.notlewx.bossbar;
+
+import com.andrei1058.bedwars.api.BedWars;
+import me.notlewx.bossbar.listeners.ArenaListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bossbar extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        if (Bukkit.getPluginManager().getPlugin("BedWars1058") == null) {
+            getLogger().severe("BedWars1058 was not found. Disabling...");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+        else {
+            System.out.println("BedWars1058 Found! Hooking...");
+        }
+
+        BedWars bedwarsAPI = Bukkit.getServicesManager().getRegistration(BedWars .class).getProvider();
+
+        getServer().getPluginManager().registerEvents(new ArenaListener(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/notlewx/bossbar/listeners/ArenaListener.java
@@ -0,0 +1,36 @@
+package me.notlewx.bossbar.listeners;
+
+import com.andrei1058.bedwars.api.events.gameplay.GameStateChangeEvent;
+import me.clip.placeholderapi.libs.kyori.adventure.platform.facet.Facet;
+import org.bukkit.boss.BossBar;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.event.EventHandler;
+import static com.andrei1058.bedwars.api.arena.GameState.*;
+import com.andrei1058.bedwars.api.arena.IArena;
+import com.andrei1058.bedwars.api.events.player.PlayerJoinArenaEvent;
+import com.andrei1058.bedwars.api.arena.GameState;
+
+public class ArenaListener implements Listener {
+
+    @EventHandler
+    public static void onArenaJoinEvent(PlayerJoinArenaEvent e) {
+        IArena arena = e.getArena();
+        Player p = e.getPlayer();
+        GameState ArenaStatus = arena.getStatus();
+
+        String message = (ChatColor.translateAlternateColorCodes('&', ChatColor.WHITE + "&lPlaying " + ChatColor.YELLOW + ChatColor.BOLD + "BED WARS" + ChatColor.WHITE + ChatColor.BOLD + " on " + ChatColor.AQUA + ChatColor.BOLD + "MC.EXAMPLE.NET"));
+        BarColor color = BarColor.PURPLE;
+        BarStyle style = BarStyle.SOLID;
+        BossBar bar = Bukkit.createBossBar(message, color, style);
+
+        if (ArenaStatus == waiting || ArenaStatus == starting) {
+            bar.addPlayer(p);
+            bar.setVisible(true);
+        }
+    }
+}
