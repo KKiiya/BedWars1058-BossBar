77df3ac2db86ef1b3458d15927146e040cfe5b30
NotLewUwU <abderrahmanlabiod@iessantiagohernadez.com>
2022-09-17 17:53:35 +0200

    Added bossbar at the end of the game

--- src/main/java/me/notlewx/bossbar/listeners/GameEnd.java
+++ src/main/java/me/notlewx/bossbar/listeners/GameEnd.java
@@ -1,5 +1,6 @@
 package me.notlewx.bossbar.listeners;
 
+import com.andrei1058.bedwars.api.arena.IArena;
 import com.andrei1058.bedwars.api.events.gameplay.GameEndEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
@@ -7,14 +8,14 @@ import org.bukkit.event.EventHandler;
 import static me.notlewx.bossbar.listeners.ArenaJoin.bar;
 
 public class GameEnd implements Listener {
-    @EventHandler
-    public static void onGameWin(GameEndEvent e) {
-        Player Win = (Player) e.getWinners();
-        bar.addPlayer(Win);
-        bar.setVisible(true);
 
-        Player Lose = (Player) e.getLosers();
-        bar.addPlayer(Lose);
-        bar.setVisible(true);
+
+    @EventHandler
+    public static void onGameEnd(GameEndEvent e) {
+         IArena arena = e.getArena();
+         for (Player player : arena.getPlayers()) {
+             bar.addPlayer(player);
+             bar.setVisible(true);
+         }
     }
 }
