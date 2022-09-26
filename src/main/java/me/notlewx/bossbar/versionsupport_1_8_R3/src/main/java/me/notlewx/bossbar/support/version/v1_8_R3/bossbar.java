b84fa3c3eff46670ba05b89034ddc919ec4f34ad
NotLewUwU <abderrahmanlabiod@iessantiagohernadez.com>
2022-09-26 16:45:30 +0200

    Moved files (again)

---
+++ src/main/java/me/notlewx/bossbar/versionsupport_1_8_R3/src/main/java/me/notlewx/bossbar/support/version/v1_8_R3/bossbar.java
@@ -0,0 +1,7 @@
+package me.notlewx.bossbar.versionsupport_1_8_R3.src.main.java.me.notlewx.bossbar.support.version.v1_8_R3;
+
+import org.bukkit.event.Listener;
+
+public class bossbar implements Listener {
+
+}
