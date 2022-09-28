2a287cd0e79cc942321ff51533d83849c3018014
NotLewUwU <abderrahmanlabiod@iessantiagohernadez.com>
2022-09-28 07:51:34 +0200

    Moved all the files, added nms to pom and fixed code

--- src/main/java/me/notlewx/bossbar/versionsupport_1_8_R3/src/main/java/me/notlewx/bossbar/support/version/v1_8_R3/bossbar.java
+++
@@ -1,7 +0,0 @@
-package me.notlewx.bossbar.versionsupport_1_8_R3.src.main.java.me.notlewx.bossbar.support.version.v1_8_R3;
-
-import org.bukkit.event.Listener;
-
-public class bossbar implements Listener {
-
-}
